1fad54504460882246971cf857acac68764603bd
AndroidGeeksYT <xavierlibresanora@gmail.com>
2025-04-09 22:23:26 +0800

    updates

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,19 +1,20 @@
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons",
-		"MunifTanjim/nui.nvim",
-	},
-	config = function()
-		require("nvim-treesitter.configs").setup({
-			ensure_installed = { "lua", "html", "css" },
-			highlight = { enable = true },
-			indent = { enable = true },
-		})
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+  },
+  lazy = false,
+  priority = 1000,
+  config = function()
+    require("nvim-treesitter.configs").setup({
+      highlight = { enable = true },
+      indent = { enable = true },
+    })
 
-		vim.keymap.set("n", "<leader>nt", ":Neotree toggle<CR>", {})
-		vim.keymap.set("n", "<leader>ntbf", ":Neotree buffers reveal float<CR>", {})
-	end,
+    vim.keymap.set("n", "<leader>t", ":Neotree toggle<CR>", {})
+    vim.keymap.set("n", "<leader>tf", ":Neotree buffers reveal float<CR>", {})
+  end,
 }
